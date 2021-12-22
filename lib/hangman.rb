998292a0f0d4949c1cce43ec8d9bf2a5a3d4f928
Orla <orladunlop2@gmail.com>
2021-12-22 12:34:54 +0000

    Blocks the same letter being guessed more than once

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -3,12 +3,21 @@ require_relative 'dictionary'
 class Hangman 
   WORD = Dictionary.new
   attr_accessor :letter
-  
+
+  def initialize
+    @guesses = []
+  end
+
   def guess(letter)
-    @letter = letter
+    if @guesses.include?(letter)
+      return "This letter has already been guessed choose another"
+    else
+      @guesses << letter
+      @letter = letter
+    end
   end
 
   def check
-    WORD.generate.chars.include?(@letter)
+    WORD.generate.include?(@letter)
   end
 end
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -11,4 +11,9 @@ describe Hangman do
     subject.guess("a")
     expect(subject.check).to eq true
   end
+
+  it 'it does not accept a letter which has already been guessed' do
+    subject.guess("a")
+    expect(subject.guess("a")).to eq "This letter has already been guessed choose another"
+  end
 end
